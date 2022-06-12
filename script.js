cdbaa1cd4a500f4f8ebf32e58446b084d2228de5
ram25493 <ram.official25@gmail.com>
2022-06-12 17:13:52 +0530

    changes done

--- script.js
+++ script.js
@@ -1,37 +1,131 @@
-function check(){
- 
-    document.getElementById("button").style.display = "";
-    document.getElementById("button").style.display = "none";
-    
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
 
-    var correct = 0;
-     
-    if (question1 == "Javascript"){
-        correct++;
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
-    if (question2 == "Cascading Style Sheets") {
-         correct++;
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function displayQuestion() {
+    if(quiz.isEnded()) {
+        showScores();
     }
-     if (question3 == "Hypertext Markup Language") {
-         correct++
-     }  
-     if (question4 == "1995") {
-        correct++
-    }  
-    if (question5 == "both") {
-        correct++
-    }  
-        quiz.innerHTML = `<button onclick="myFunction()">Restart</button>`;
-     document.getElementById("after_submit").style.visibility="visible";
-       document.getElementById("number_correct").innerHTML = "you got " + correct + " correct.";
-     
+    else {
+        let questionElement = document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        let choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
     }
-     
-    function myFunction() {
-        location.reload();
+};
+
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
     }
+};
+
+
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let ProgressElement = document.getElementById("progress");
+    ProgressElement.innerHTML = 
+    `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
+
+function showScores() {
+    let quizEndHTML = 
+    `
+    <h1>Quiz Completed</h1>
+    <h2 id='score'> Your scored: ${quiz.score} of ${quiz.questions.length}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Take Quiz Again</a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+};
+
+let questions = [
+    new Question(
+        "Hyper Text Markup Language Stands For?", 
+        ["JQuery", "XHTML","CSS", "HTML"], "HTML"
+    ),
+    new Question(
+        "Cascading Style sheet stands for?", 
+        ["HTML", "JQuery", "CSS", "XML"], "CSS"
+    ),
+    new Question(
+        "Which is a JavaScript Framework?", 
+        ["React", "Laravel","Django", "Sass"], "React"
+        ),
+    new Question(
+        "Which is a backend language?", 
+        ["PHP", "HTML", "React", "All"], "PHP"
+        ),
+    new Question(
+        "Which is best for Artificial intelligence?", 
+        ["React", "Laravel","Python", "Sass"], "Python"
+        )
+];
+
+
+let quiz = new Quiz(questions);
+
+displayQuestion();
+
+let time = 2;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown(){
+    let quizTimer = setInterval(function(){
+    if(quizTime <= 0) {
+        clearInterval(quizTimer);
+        showScores();
+    } else {
+        quizTime--;
+        let sec = Math.floor(quizTime % 60);
+        let min = Math.floor(quizTime / 60) % 60;
+        counting.innerHTML = `TIME: ${min} : ${sec}`;   
+    }
+},1000);
+}
+
+startCountdown();
